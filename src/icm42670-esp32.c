9db32db924a4818f850c339b4e0e6356e57878cc
Obediah Klopfenstein <obe711@gmail.com>
2025-09-07 10:46:31 -0500

    Set log level

--- src/icm42670-esp32.c
+++ src/icm42670-esp32.c
@@ -22,7 +22,7 @@
 
 // static i2c_port_t i2c_master_port = I2C_MASTER_NUM;
 
-static icm42670_handle_t icm_sensor;
+// static icm42670_handle_t icm_sensor;
 
 // icm42670_cfg_t sensor_cfg = {
 //     ACCE_FS_8G,      /*!< Accelerometer full scale range */
@@ -35,86 +35,12 @@ float calculate_tilt_angle(float x, float y, float z) {
   return fabsf(atan2f(x, sqrtf(y * y + z * z)) * RAD_TO_DEG);
 }
 
-// static void imu_task(void *pvParameter) {
+esp_err_t set_icm42670_log_level(esp_log_level_t log_level) {
+  esp_log_level_set("ICM42670", log_level);
+  esp_log_level_set(TAG, log_level);
 
-//   // float sensitivity;
-
-//   // ret = icm42670_get_acce_sensitivity(icm_sensor, &sensitivity);
-//   // if (ret != ESP_OK)
-//   // {
-//   //   ESP_LOGE(TAG, "icm42670_get_acce_sensitivity");
-//   // }
-//   // ESP_LOGI(TAG, "acce_sensitivity: %.2f", sensitivity);
-
-//   // ret = icm42670_get_gyro_sensitivity(icm_sensor, &sensitivity);
-//   // if (ret != ESP_OK)
-//   // {
-//   //   ESP_LOGE(TAG, "icm42670_get_gyro_sensitivity");
-//   // }
-//   // ESP_LOGI(TAG, "gyro_sensitivity: %.2f", sensitivity);
-
-//   icm42670_value_t acce_value = {0};
-//   icm42670_value_t gyro_value = {0};
-//   complimentary_angle_t comp_angle;
-
-//   float tilt_angle;
-//   bool is_tilted = false;
-
-//   if (icm42670_get_acce_value(icm_sensor, &acce_value) != ESP_OK) {
-//     ESP_LOGE(TAG, "icm42670_get_acce_value");
-//   };
-
-//   if (icm42670_get_gyro_value(icm_sensor, &gyro_value) != ESP_OK) {
-//     ESP_LOGE(TAG, "icm42670_get_gyro_value");
-//   };
-
-//   // if (icm42670_complimentory_filter(icm_sensor, &acce_value, &gyro_value,
-//   // &comp_angle) != ESP_OK)
-//   // {
-//   //   ESP_LOGE(TAG, "icm42670_complimentory_filter");
-//   // };
-
-//   while (1) {
-
-//     if (icm42670_get_acce_value(icm_sensor, &acce_value) != ESP_OK) {
-//       ESP_LOGE(TAG, "icm42670_get_acce_value");
-//     };
-
-//     if (icm42670_get_gyro_value(icm_sensor, &gyro_value) != ESP_OK) {
-//       ESP_LOGE(TAG, "icm42670_get_gyro_value");
-//     };
-
-//     // if (icm42670_complimentory_filter(icm_sensor, &acce_value,
-//     &gyro_value,
-//     // &comp_angle) != ESP_OK)
-//     // {
-//     //   ESP_LOGE(TAG, "icm42670_complimentory_filter");
-//     // };
-
-//     tilt_angle = calculate_tilt_angle(acce_value.x, acce_value.y,
-//     acce_value.z); if (!is_tilted && tilt_angle > TILT_THRESHOLD) {
-//       printf("Tilt detected! Angle: %.2f degrees\n", tilt_angle);
-//       is_tilted = true;
-//     } else if (is_tilted && tilt_angle < RETURN_THRESHOLD) {
-//       printf("Object returned to position. Angle: %.2f degrees\n",
-//       tilt_angle); is_tilted = false;
-//     }
-//     ESP_LOGD(TAG, "Tilt angle: %.2f", tilt_angle);
-
-//     // ESP_LOGD(TAG, "acce: X = %.2f, Y = %.2f, Z = %.2f", acce_value.x,
-//     // acce_value.y, acce_value.z); ESP_LOGD(TAG, "gyro: X = %.2f, Y = %.2f,
-//     Z =
-//     // %.2f", gyro_value.x, gyro_value.y, gyro_value.z); ESP_LOGD(TAG, "comp:
-//     // pitch = %.2f, roll = %.2f \n", comp_angle.pitch, comp_angle.roll);
-
-//     // ESP_LOGD(TAG, "Y: %.2f - %.2f", gyro_value.y, acce_value.y);
-
-//     // ESP_LOGI(TAG, "initial_pitch: %.2f, current_pitch: %.2f, delta: %.2f",
-//     // initial_pitch, current_pitch, delta_pitch);
-
-//     vTaskDelay(pdMS_TO_TICKS(100));
-//   }
-// }
+  return ESP_OK;
+};
 
 esp_err_t init_icm42670(icm42670_esp_config_t *esp_config) {
   ESP_LOGI(TAG, "Init icm42670");
@@ -143,10 +69,6 @@ esp_err_t init_icm42670(icm42670_esp_config_t *esp_config) {
     ESP_LOGE(TAG, "icm42670_gyro_set_pwr");
     return ESP_FAIL;
   }
-
-  // vTaskDelay(pdMS_TO_TICKS(20));
-
-  // xTaskCreate(imu_task, "imu_task", 4095, NULL, 5, NULL);
 
   return ESP_OK;
 }
